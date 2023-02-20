4665da7bb8a9d3adfddf3b53b95e8b8a2f26e6b2
Jobayer Mannan <jobayermannan777@gmail.com>
2023-02-20 11:29:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ // for first card
+  document.getElementById("first-card").addEventListener("click", function () {
+   
+//    get the data  from html using id
+    const firstElement  = document.getElementById("first-name").value;
+    
+  
+    
+  });
+   
